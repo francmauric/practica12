6b94d31296fa40988f905f4a44324723cbaeac08
franc_mauric1 <franc_mauric1@hotmail.com>
2024-05-25 18:11:20 +0200

    test 2

---
+++ test/App.test.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { describe, test, expect } from 'vitest'
+import { render, screen } from '@testing-library/react'
+import App from '../src/App'
+
+
+describe('<App />', () => {
+    test('should work', () => {
+        render(<App />)
+        
+
+         expect(
+           screen.getByText('Prueba técnica de React')
+         ).toBeDefined()
+
+      })
+    })
